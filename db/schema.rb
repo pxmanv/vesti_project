1bde6e42e9149a9227115bde57d73ba9e0bf3ae2
Peter Christman <peter.christman@gmail.com>
2014-12-10 17:36:12 -0600

    Update v9

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141203050718) do
+ActiveRecord::Schema.define(version: 20141209220907) do
 
   create_table "accessories", force: true do |t|
     t.integer  "name_id"
@@ -96,6 +96,7 @@ ActiveRecord::Schema.define(version: 20141203050718) do
     t.integer  "accessory_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "seasons", force: true do |t|
